13f53f525de298bbe1113f35c492cc5680966653
admin <tmdco12@naver.com>
2023-12-27 09:28:00 +0900

    test - 이체기능 검증 테스트

---
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,30 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.repository.MemberRepositoryV1;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+/**
+ * 기본 동작, 트랜잭션이 없어서 문제 발생
+ */
+public class MemberServiceV1Test {
+
+    // 테스트에 사용될 회원 ID 상수
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    // 각 테스트 메서드 실행전 초기화(데이터베이스 연결 정보와 서비스 객체를 초기화한다.)
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+    }
+}
