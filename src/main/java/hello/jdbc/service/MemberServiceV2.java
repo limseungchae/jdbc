5f0bfb63504a07bd3fef3d410e3de0c8ef89d58a
admin <tmdco12@naver.com>
2023-12-28 07:57:07 +0900

    service - 트랜잭션 - 파라미터 연동, 풀을 고려한 종료

---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,40 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
+        // 이체를 하는 회원과 받는 회원의 정보 조회
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        // 이체를 하는 회원의 잔액 업데이트
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        // 오류 케이스
+        validation(toMember);
+
+        // 이체를 받는 회원의 잔액 업데이트
+        memberRepository.update(toId, toMember.getMoney() + money);
+        // 커밋, 롤백
+    }
+
+    // 오류 케이스를 검증하는 메서드.
+    private static void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("에치중 예외 발생");
+        }
+    }
+}
