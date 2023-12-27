ed243c6562cf7f821a0de1b573fa1af8d35bcfd6
admin <tmdco12@naver.com>
2023-12-27 17:14:05 +0900

    test - 정상이체하는 로직 검증

--- src/test/java/hello/jdbc/service/MemberServiceV1Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.service;
 import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV1;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * 기본 동작, 트랜잭션이 없어서 문제 발생
@@ -46,5 +48,9 @@ public class MemberServiceV1Test {
         memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
 
         //then (검증)
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
     }
 }
