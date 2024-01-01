0def2ab8ae200dbb7d639128b8516ddb786dcc8f
admin <tmdco12@naver.com>
2024-01-02 08:59:57 +0900

    service - release 메서드 제거

--- src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -54,18 +54,6 @@ public class MemberServiceV3_1 {
         memberRepository.update(toId, toMember.getMoney() + money);
     }
 
-    // 커넥션을 해제하고 자동 커밋을 활성화하는 메서드
-    private static void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); // 커넥션 풀 고려
-                con.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            }
-        }
-    }
-
     // 오류 케이스를 검증하는 메서드.
     private static void validation(Member toMember) {
         if(toMember.getMemberId().equals("ex")) {
