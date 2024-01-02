51d5cc2640ede41b4cd89891356996d9f2edb51c
admin <tmdco12@naver.com>
2024-01-02 13:15:52 +0900

    service - 템플릿 제거

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -3,9 +3,6 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
-
 import java.sql.SQLException;
 
 /**
@@ -14,25 +11,15 @@ import java.sql.SQLException;
 @Slf4j
 public class MemberServiceV3_3 {
 
-//    private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_3(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        txTemplate.executeWithoutResult((status) -> {
             // 비지니스 로직
-            try {
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+            bizLogic(fromId, toId, money);
     }
 
     // 비지니스 로직
