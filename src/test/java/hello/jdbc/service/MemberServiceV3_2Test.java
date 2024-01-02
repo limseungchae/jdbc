0c8b71fafc31e3cf5b630abed88edec06d7afde9
admin <tmdco12@naver.com>
2024-01-02 10:31:53 +0900

    tset - 인터페이스 변경

--- src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -29,7 +29,7 @@ public class MemberServiceV3_2Test {
     public static final String MEMBER_EX = "ex";
 
     private MemberRepositoryV3 memberRepository;
-    private MemberServiceV3_1 memberService;
+    private MemberServiceV3_2 memberService;
 
     // 각 테스트 메서드 실행전 초기화(데이터베이스 연결 정보와 서비스 객체를 초기화한다.)
     @BeforeEach
@@ -37,7 +37,7 @@ public class MemberServiceV3_2Test {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         memberRepository = new MemberRepositoryV3(dataSource);
         PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-        memberService = new MemberServiceV3_1(transactionManager, memberRepository);
+        memberService = new MemberServiceV3_2(transactionManager, memberRepository);
     }
 
     // 각각의 테스트 끝날때 after 호출 (리소스 정리)
