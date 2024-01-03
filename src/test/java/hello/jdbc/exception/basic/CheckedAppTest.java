064da6b275d31e424b99b1926183a1cb8fa7196f
admin <tmdco12@naver.com>
2024-01-04 05:34:08 +0900

    test - 체크 예외 활용

---
+++ src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -0,0 +1,14 @@
+package hello.jdbc.exception.basic;
+
+public class CheckedAppTest {
+
+    static class Service {
+
+    }
+    static class NetworkClient {
+
+    }
+    static class Repository {
+
+    }
+}
