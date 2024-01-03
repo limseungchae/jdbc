daae93a95178a9ee9f8c062207f91feb59d36b98
admin <tmdco12@naver.com>
2024-01-04 06:08:50 +0900

    test - Service 호출 repository,NetworkClient 예외 선언

--- src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -3,18 +3,29 @@ package hello.jdbc.exception.basic;
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+import static hello.jdbc.exception.basic.CheckedAppTest.NetworkClient.*;
+
 public class CheckedAppTest {
 
     static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
 
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            networkClient.call();
+        }
     }
+
     static class NetworkClient {
         public void call() throws ConnectException {
             throw new ConnectException("연결 실패");
-    }
-    static class Repository {
-        public void call() throws SQLException {
-            throw new SQLException("ex");
+        }
+
+        static class Repository {
+            public void call() throws SQLException {
+                throw new SQLException("ex");
+            }
         }
     }
 }
