34f46786ab2261f7e030809927a5c3ddb829e915
admin <tmdco12@naver.com>
2024-01-03 23:05:26 +0900

    test - 언체크 예외 RuntimeException을 상속받은 예외는 언체크 예외가 된다.

--- src/test/java/hello/jdbc/exception/basic/UnckeckedTset.java
+++ src/test/java/hello/jdbc/exception/basic/UnckeckedTset.java
@@ -2,4 +2,13 @@ package hello.jdbc.exception.basic;
 
 public class UnckeckedTset {
 
+    /**
+     * RuntimeException을 상속받은 예외는 언체크 예외가 된다.
+     */
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
 }
