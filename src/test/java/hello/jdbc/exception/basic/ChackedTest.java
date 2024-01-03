be26749b126ea1d411413103ef52bdd87079ee63
admin <tmdco12@naver.com>
2024-01-03 22:09:27 +0900

    test - Exception 상속받으면 체크 예외 컴파일러가 체크하는 체크예외

--- src/test/java/hello/jdbc/exception/basic/ChackedTest.java
+++ src/test/java/hello/jdbc/exception/basic/ChackedTest.java
@@ -1,4 +1,16 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ChackedTest {
+
+    /**
+     * Exception을 상속받은 예외는 체크 예외가 된다.
+     */
+    static class MyCheckedExeption extends Exception {
+        public MyCheckedExeption(String message) {
+            super(message);
+        }
+    }
 }
