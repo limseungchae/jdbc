77cb04e675e0c4253f8feba48ff420af8868b0f8
admin <tmdco12@naver.com>
2024-01-06 10:46:12 +0900

    service - SQLException 제거

--- src/main/java/hello/jdbc/service/MemberServiceV4.java
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -2,11 +2,8 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.SQLException;
 
 /**
  * 예외 누수 문제 해결
@@ -24,13 +21,13 @@ public class MemberServiceV4 {
     }
 
     @Transactional
-    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+    public void accountTransfer(String fromId, String toId, int money) {
             // 비지니스 로직
             bizLogic(fromId, toId, money);
     }
 
     // 비지니스 로직
-    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+    private void bizLogic(String fromId, String toId, int money) {
         // 이체를 하는 회원과 받는 회원의 정보 조회
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
