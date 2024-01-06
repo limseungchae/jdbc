567d577882357ecc208fe21897fb2363135c0b65
admin <tmdco12@naver.com>
2024-01-06 10:44:42 +0900

    service - 런타임 예외 적용

---
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,53 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ *
+ * MemberRepository 인터페이스 의존
+ */
+@Slf4j
+public class MemberServiceV4 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV4(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+            // 비지니스 로직
+            bizLogic(fromId, toId, money);
+    }
+
+    // 비지니스 로직
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        // 이체를 하는 회원과 받는 회원의 정보 조회
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        // 이체를 하는 회원의 잔액 업데이트
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        // 오류 케이스
+        validation(toMember);
+
+        // 이체를 받는 회원의 잔액 업데이트
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    // 오류 케이스를 검증하는 메서드.
+    private static void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("에치중 예외 발생");
+        }
+    }
+}
