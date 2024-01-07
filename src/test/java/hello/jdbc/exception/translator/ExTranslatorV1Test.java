1b1cfdd05b0cb7961b72a147a2c753ca8103bb8d
admin <tmdco12@naver.com>
2024-01-07 19:44:12 +0900

    test - SQL 쿼리 실행 코드 추가

--- src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -3,8 +3,12 @@ package hello.jdbc.exception.translator;
 import hello.jdbc.domain.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Slf4j
 public class ExTranslatorV1Test {
@@ -15,6 +19,23 @@ public class ExTranslatorV1Test {
 
         public Member save(Member member) {
             String sql = "insert into member(member_id, money) values(?,?)";
+            Connection con = null;
+            PreparedStatement pstmt = null;
+
+            try {
+                con = dataSource.getConnection();
+                pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, member.getMemberId());
+                pstmt.setInt(2, member.getMoney());
+                pstmt.executeUpdate();
+                return member;
+            } catch (SQLException e) {
+
+            } finally {
+                JdbcUtils.closeStatement(pstmt);
+                JdbcUtils.closeConnection(con);
+
+            }
         }
 
     }
