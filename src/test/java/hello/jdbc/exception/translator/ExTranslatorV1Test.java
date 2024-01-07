3b4988ea4c57969e6063e43445e5cb5f7882cc16
admin <tmdco12@naver.com>
2024-01-07 20:11:40 +0900

    test - 키 중복인 경우 예외가 올라옴

--- src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -1,11 +1,14 @@
 package hello.jdbc.exception.translator;
 
 import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
+import java.security.KeyException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -30,6 +33,10 @@ public class ExTranslatorV1Test {
                 pstmt.executeUpdate();
                 return member;
             } catch (SQLException e) {
+                // h2 db
+                if (e.getErrorCode() == 23505) {
+                    throw new MyDbException(e);
+                }
 
             } finally {
                 JdbcUtils.closeStatement(pstmt);
