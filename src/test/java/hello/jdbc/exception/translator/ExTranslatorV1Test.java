82c613dccc78e7b52befbe4c8854eba11c1fe79b
admin <tmdco12@naver.com>
2024-01-07 19:20:50 +0900

    test - 리포지토리 생성

--- src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -1,4 +1,13 @@
 package hello.jdbc.exception.translator;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ExTranslatorV1Test {
+
+    @RequiredArgsConstructor
+    static class Repository {
+
+    }
 }
