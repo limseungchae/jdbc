085c30151985c9d1f4a34c5fea9cf790849427eb
admin <tmdco12@naver.com>
2024-01-07 19:22:46 +0900

    test - dataSource 주입

--- src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -3,11 +3,14 @@ package hello.jdbc.exception.translator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.sql.DataSource;
+
 @Slf4j
 public class ExTranslatorV1Test {
 
     @RequiredArgsConstructor
     static class Repository {
+        private final DataSource dataSource;
         String sql = "insert into member(member_id, money) values(?,?)";
 
     }
